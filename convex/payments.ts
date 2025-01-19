e97dda87f0a9897c337786fed6ea2a61dab96e78
DivineDemon <supame123@gmail.com>
2025-01-20 01:42:20 +0500

    Fixes!

--- convex/payments.ts
+++ convex/payments.ts
@@ -3,45 +3,58 @@ import { internalMutation } from "./_generated/server";
 import { updateUserCredits } from "./users";
 
 export const createEmptyPaymentRecord = internalMutation({
-    handler: async (ctx) => {
-        const identity = await ctx.auth.getUserIdentity();
-        if (!identity) {
-            return null;
-        }
-        const { subject } = identity;
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      return null;
+    }
+    const { subject } = identity;
 
-        return await ctx.db.insert("payments", { userId: subject, stripeId: "", status: "initiated" });
-    },
+    return await ctx.db.insert("payments", {
+      userId: subject,
+      email: "",
+      stripeId: "",
+      paymentId: "",
+      phone: "",
+      amount: 0,
+      created_at: 0,
+      method: "",
+      status: "initiated",
+      currency: "",
+    });
+  },
 });
 
 export const markPending = internalMutation({
-    args: {
-        paymentId: v.id("payments"),
-        stripeId: v.string(),
-    },
-    handler: async (ctx, { paymentId, stripeId }) => {
-        await ctx.db.patch(paymentId, { stripeId, status: "processing" });
-    },
+  args: {
+    paymentId: v.id("payments"),
+    stripeId: v.string(),
+  },
+  handler: async (ctx, { paymentId, stripeId }) => {
+    await ctx.db.patch(paymentId, { stripeId, status: "processing" });
+  },
 });
 
 export const fulfill = internalMutation({
-    args: { stripeId: v.string(), status: v.string() },
-    handler: async (ctx, { stripeId, status }) => {
-        const { db, } = ctx;
-        const { _id, userId } = (await db
-            .query("payments")
-            .withIndex("by_stripe_id", (q) => q.eq("stripeId", stripeId))
-            .unique())!;
+  args: { stripeId: v.string(), status: v.string() },
+  handler: async (ctx, { stripeId, status }) => {
+    const { db } = ctx;
+    const { _id, userId } = (await db
+      .query("payments")
+      .withIndex("by_paymentId", (q) => q.eq("paymentId", stripeId))
+      .unique())!;
 
-        if (!_id) {
-            console.log("error finding the payment id to update the status after fullfilling the payemnt");
-            return null;
-        }
+    if (!_id) {
+      console.log(
+        "error finding the payment id to update the status after fullfilling the payemnt"
+      );
+      return null;
+    }
 
-        await ctx.db.patch(_id, { status });
+    await ctx.db.patch(_id, { status });
 
-        await updateUserCredits(ctx, userId);
-    },
+    await updateUserCredits(ctx, "", 0, userId);
+  },
 });
 
 // export const authoRizedRazorPay = internalMutation({
