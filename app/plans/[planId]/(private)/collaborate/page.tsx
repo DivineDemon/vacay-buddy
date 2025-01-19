00f88c4f5e640ec06c3e172fba66043eccc8c231
DivineDemon <supame123@gmail.com>
2025-01-20 01:08:50 +0500

    Fixes!

--- app/plans/[planId]/(private)/collaborate/page.tsx
+++ app/plans/[planId]/(private)/collaborate/page.tsx
@@ -1,5 +1,26 @@
-import Collaborator from "@/components/settings/Collaborator";
+import AccessRecords from "@/components/settings/AccessRecords";
+import InviteForm from "@/components/settings/InviteForm";
+import PendingInvites from "@/components/settings/PendingInvites";
 
-export default function Collaborate() {
-  return <Collaborator />;
+interface PageProps {
+  params: Promise<{ planId: string }>;
+}
+
+export default async function Collaborate(props: PageProps) {
+  const { planId } = await props.params;
+
+  return (
+    <div className="bg-background shadow-sm rounded-lg p-4 border-2 border-border">
+      <div className="border-b-2 border-b-border pb-2 mb-2 font-bold">
+        Collaborators
+      </div>
+      <p className="text-sm text-muted-foreground">
+        To invite people to your travel plan, send them a email invite using
+        below
+      </p>
+      <InviteForm planId={planId} />
+      <PendingInvites planId={planId} />
+      <AccessRecords planId={planId} />
+    </div>
+  );
 }
