9d4ec36b60f932db56ac2b3a1f8d80a64e043b3c
DivineDemon <supame123@gmail.com>
2025-01-20 01:12:37 +0500

    Fixes!

--- app/plans/[planId]/(private)/expense-tracker/page.tsx
+++ app/plans/[planId]/(private)/expense-tracker/page.tsx
@@ -1,7 +1,11 @@
 import ExpenseSection from "@/components/expenseTracker/ExpenseSection";
 
-export default async function ExpenseTracker({params}: {params: {planId: string}}) {
-  const {planId} = params;
+interface PageProps {
+  params: Promise<{ planId: string }>;
+}
+
+export default async function ExpenseTracker(props: PageProps) {
+  const { planId } = await props.params;
 
   return <ExpenseSection planId={planId} />;
 }
